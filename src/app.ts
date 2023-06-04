611ef82405d5d4a40d6025f725c994f86107b04e
ramiro <ramiroparraperez@gmail.com>
2023-06-04 11:54:14 -0300

    add cron

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,19 @@
 import { sequelize } from './database.js';
 import { fetchFeeds } from './fetchFeeds.js';
+import { CronJob } from 'cron';
+
 import "./tgBot.js";
 
-// sequelize.sync().then(() => {
-//   console.log('Database sync complete.');
+sequelize.sync().then(() => {
+  console.log('Database sync complete.');
 
-//   setInterval(fetchFeeds, 5 * 60 * 1000); // every 5 minutes
-// });
+  // setInterval(fetchFeeds, 5 * 60 * 1000); // every 5 minutes
+});
+
+
+// schedule for reminders
+const remindersJob = new CronJob('*/5 * * * *', async () => {
+  fetchFeeds();
+}, null, true, "America/Paramaribo");
+
+remindersJob.start();
