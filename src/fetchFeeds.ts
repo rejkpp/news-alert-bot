12175cec4720ef12f11c6f1abf35d8b9c0355193
ramiro <ramiroparraperez@gmail.com>
2024-12-14 19:45:32 -0500

    Update fetchFeeds.ts

--- src/fetchFeeds.ts
+++ src/fetchFeeds.ts
@@ -24,6 +24,16 @@ interface Item {
 
 const adminGroup = Number(process.env.GROUP_ID_ADMIN);
 
+const fetchOptions: RequestInit = {
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1'
+  }
+};
+
 const parser = new Parser({
   headers: {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
@@ -73,16 +83,6 @@ async function scanFeeds(feeds: Record<string, string>) {
         // ======================
         // USE FETCH TO GET FEED
         // ======================
-        const fetchOptions: RequestInit = {
-          headers: {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-            'Accept-Language': 'en-US,en;q=0.5',
-            'Connection': 'keep-alive',
-            'Upgrade-Insecure-Requests': '1'
-          }
-        };
-
         const response = await fetch(feed, fetchOptions);
 
         if (response.status < 200 || response.status >= 600) {
